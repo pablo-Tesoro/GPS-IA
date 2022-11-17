68405b18cc784bb2614cd8666616693df23650f4
pablo-Tesoro <tesoroatleti7@gmail.com>
2022-11-17 21:48:02 +0100

    grafo

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import networkx as nx
-from Algoritmo import *
+import pandas as pd
+import numpy as np
 
-G = nx.Graph()
-#holaa
+edges = pd.read_csv('metro.csv', index_col=None, encoding='cp1252')
+
+G = nx.from_pandas_edgelist(edges, edge_attr=['Distancia(Km)', 'Tiempo (min)'])
+
+G.nodes()
+G.edges()
+G.order()
+
+for x in G.nodes():
+    if G.degree(x) > 2:
+        print(x)
+        
+        
