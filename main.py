0f07c50e9266287670c57044564de8567dc84246
pablo-Tesoro <tesoroatleti7@gmail.com>
2022-11-17 23:12:50 +0100

    grafo

--- main.py
+++ main.py
@@ -2,16 +2,6 @@ import networkx as nx
 import pandas as pd
 import numpy as np
 
-edges = pd.read_csv('metro.csv', index_col=None, encoding='cp1252')
+data = pd.read_csv('metro.csv', sep=';', index_col=False, encoding='cp1252')
 
-G = nx.from_pandas_edgelist(edges, edge_attr=['Distancia(Km)', 'Tiempo (min)'])
-
-G.nodes()
-G.edges()
-G.order()
-
-for x in G.nodes():
-    if G.degree(x) > 2:
-        print(x)
-        
-        
+G = nx.from_pandas_edgelist(data, source='ORIGEN', target='DESTINO', edge_attr=['DISTANCIA', 'TIEMPO'])
