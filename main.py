fb8bcec211895bcf13062fe10acd9c92a3807b6c
adrian raposo <adrianraposopozuelo@gmail.com>
2022-11-19 00:34:00 +0100

    Algoritmo A* hecho

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 import networkx as nx
 import pandas as pd
 import numpy as np
+from Algoritmo import Alg
+import matplotlib.pyplot as plt
+import json
+import os
 
-data = pd.read_csv('metro.csv', sep=';', index_col=False, encoding='cp1252')
+def leer(data):
+        with open (data, 'r') as f:
+            coords = json.load(f)
+            f.close()
+        return coords
+
+data = pd.read_csv(os.path.abspath("Pruebas\metro.csv"), sep=';', index_col=False, encoding='cp1252')
+
+A = nx.Graph()
 
 G = nx.from_pandas_edgelist(data, source='ORIGEN', target='DESTINO', edge_attr=['DISTANCIA', 'TIEMPO'])
+
+
+coords = leer(os.path.abspath("Pruebas\Coordenadas.json"))
+for x in coords:
+    G.nodes[x['Station']]['Coordenadas'] = [x['Latitude'], x['Longitude']]
+    G.nodes[x['Station']]['Linea'] = x['Line']
+
+al = Alg(G, 'Piraeus',  'Aghios Antonios')
+al.algoritmo()
+print(al.listaCerrada)
+al.camino()
+print(al.recorrido)
+print(al.distancia)
+al.tiempo()
+print(al.tiempomin)
